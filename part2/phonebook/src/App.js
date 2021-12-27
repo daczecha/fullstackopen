c8b128ee0bb1db47bccd25f4388c329034b82cf0
daczecha <daczecha@gmail.com>
2021-12-27 19:32:07 +0400

    phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
+
+import axios from 'axios';
+
 import Numbers from './components/Numbers';
 import New from './components/New';
 import Filter from './components/Filter';
 
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
 
 
@@ -52,6 +52,12 @@ const App = () => {
     setNewNumber('');
   }
 
+
+  useEffect(()=>{
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+  },[]);
 
   return (
     <div>
