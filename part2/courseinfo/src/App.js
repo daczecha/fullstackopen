e9448f8ec7c6822fe6548bbbedd1944fff44d455
daczecha <daczecha@gmail.com>
2021-12-27 09:55:00 +0400

    courseinfo separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,46 +1,4 @@
-function Part(props){
-  return(
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  );
-}
-
-function Header(props){
-  return(
-    <h3>{props.course}</h3>
-  );
-}
-
-function Content(props){
-  return(
-    <>
-      {props.parts.map((part)=><Part part={part.name} exercise={part.exercises} key={part.id}/>)}
-    </>
-  );
-}
-
-function Total(props){
-  let a = props.parts.map((part)=>{
-    return part.exercises;
-  });
-  const total = a.reduce(function(a, b) { return a + b; }, 0);
-  return(
-    <b>total of {total} exercises</b>
-  );
-}
-
-
-
-function Course(props){
-  return(
-  <>
-    <Header course = {props.course.name}/>
-    <Content parts = {props.course.parts}/>
-    <Total parts = {props.course.parts}/>
-  </>
-  );
-}
+import Course from "./Course";
 
 function App() {
   const courses = [
