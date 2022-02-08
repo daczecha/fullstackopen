89dc21418acc08e3fc8180e24181ae92e98b3e3e
daczecha <daczecha@gmail.com>
2022-02-08 17:27:02 +0400

    helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((a, b) => a + (b.likes || 0), 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length < 1) return {};
+
+  return blogs.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
