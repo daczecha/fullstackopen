e3b85a130d416478fb485dbce6e0569f9f2f2e18
daczecha <daczecha@gmail.com>
2022-02-08 15:58:29 +0400

    bloglist step1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,48 +1,41 @@
 require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
 const mongoUrl = process.env.MONGO_URI;
-mongoose.connect(mongoUrl)
+mongoose.connect(mongoUrl);
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'));
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  
-  console.log(request.body);
-  
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+  const blog = new Blog(request.body);
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
