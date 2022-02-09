1b616bd7d84bebf50f47fe57983e27c890211f6a
daczecha <daczecha@gmail.com>
2022-02-09 19:25:16 +0400

    bloglist expansions, step1

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -12,18 +12,23 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  //if likes is not defined, define it and set it to 0
-  if (request.body.title === undefined && request.body.url === undefined) {
-    response.status(400).send();
+  if (request.body.title === undefined || request.body.url === undefined) {
+    response.status(400).json({ error: 'bad request' });
+  } else {
+    //if likes is not defined, define it and set it to 0
+    const body = request.body.likes
+      ? request.body
+      : { ...request.body, likes: 0 };
+
+    const blog = new Blog(body);
+    const result = await blog.save();
+    response.status(201).json(result);
   }
+});
 
-  const body = request.body.likes
-    ? request.body
-    : { ...request.body, likes: 0 };
-
-  const blog = new Blog(body);
-  const result = await blog.save();
-  response.status(201).json(result);
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogs = await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).json(blogs);
 });
 
 module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -16,72 +16,90 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('when sending get request to /api/blogs, verify that ', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('blogs are correct amount', async () => {
-  const response = await api.get('/api/blogs');
+  test('blogs are in correct amount', async () => {
+    const response = await api.get('/api/blogs');
 
-  expect(response.body.length).toEqual(2);
-});
+    expect(response.body.length).toEqual(2);
+  });
 
-test('blogs have identifier property named "id"', async () => {
-  const response = await api.get('/api/blogs');
+  test('blogs have identifier property named "id"', async () => {
+    const response = await api.get('/api/blogs');
 
-  response.body.forEach((element) => {
-    expect(element.id).toBeDefined();
+    response.body.forEach((element) => {
+      expect(element.id).toBeDefined();
+    });
   });
 });
 
-test('blogs can be created', async () => {
-  const payload = {
-    title: 'Why does my back hurt?',
-    author: 'Mama',
-    url: 'url.com',
-    likes: 4129,
-  };
+describe('when sending post request to /api/blogs, verify that ', () => {
+  test('blogs can be created, the content matches and number of total blogs is increased', async () => {
+    const payload = {
+      title: 'Why does my back hurt?',
+      author: 'Mama',
+      url: 'url.com',
+      likes: 4129,
+    };
 
-  await api.post('/api/blogs').send(payload).expect(201);
+    await api.post('/api/blogs').send(payload).expect(201);
 
-  const response = await api.get('/api/blogs');
+    const response = await api.get('/api/blogs');
 
-  const blogs = response.body;
-  const lastBlog = blogs[blogs.length - 1];
-  delete lastBlog.id;
+    const blogs = response.body;
+    const lastBlog = blogs[blogs.length - 1];
+    delete lastBlog.id;
 
-  expect(blogs.length).toEqual(helper.initialBlogs.length + 1);
-  expect(payload).toEqual(lastBlog);
-});
+    expect(blogs.length).toEqual(helper.initialBlogs.length + 1);
+    expect(payload).toEqual(lastBlog);
+  });
 
-test('if the likes property is missing from the request, it will default to the value 0.', async () => {
-  const payload = {
-    title: 'Why does my back hurt?',
-    author: 'Mama',
-    url: 'url.com',
-  };
+  test('if the likes property is missing from the request, it will default to the value 0.', async () => {
+    const payload = {
+      title: 'Why does my back hurt?',
+      author: 'Mama',
+      url: 'url.com',
+    };
 
-  await api.post('/api/blogs').send(payload).expect(201);
+    await api.post('/api/blogs').send(payload).expect(201);
 
-  const response = await api.get('/api/blogs');
+    const response = await api.get('/api/blogs');
 
-  const blogs = response.body;
-  const lastBlog = blogs[blogs.length - 1];
+    const blogs = response.body;
+    const lastBlog = blogs[blogs.length - 1];
 
-  expect(lastBlog.likes).toBeDefined();
-  expect(lastBlog.likes).toEqual(0);
-});
+    expect(lastBlog.likes).toBeDefined();
+    expect(lastBlog.likes).toEqual(0);
+  });
 
-test('if the title and url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request.', async () => {
-  const payload = {
-    author: 'Mama',
-    likes: 13,
-  };
+  test('if the title and url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request.', async () => {
+    const payload = {
+      author: 'Mama',
+      likes: 13,
+    };
 
-  await api.post('/api/blogs').send(payload).expect(400);
+    const payloadWithUrl = {
+      author: 'Mama',
+      url: 'test.com',
+      likes: 13,
+    };
+
+    const payloadWithTitle = {
+      author: 'Mama',
+      title: 'test',
+      likes: 13,
+    };
+
+    await api.post('/api/blogs').send(payload).expect(400);
+    await api.post('/api/blogs').send(payloadWithUrl).expect(400);
+    await api.post('/api/blogs').send(payloadWithTitle).expect(400);
+  });
 });
 
 afterAll(() => {
