f7a27e9f9bb179b0eed84d8b6aae59ae8fb51bce
daczecha <daczecha@gmail.com>
2022-02-09 18:33:06 +0400

    bloglist tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -18,6 +18,7 @@ beforeEach(async () => {
 
 describe('blogs', () => {
   test('are returned as json', async () => {
+    jest.setTimeout(10000);
     await api
       .get('/api/blogs')
       .expect(200)
@@ -25,17 +26,41 @@ describe('blogs', () => {
   });
 
   test('are correct amount', async () => {
+    jest.setTimeout(10000);
     const response = await api.get('/api/blogs');
 
     expect(response.body.length).toEqual(2);
   });
 
   test('have identifier property named "id"', async () => {
+    jest.setTimeout(10000);
     const response = await api.get('/api/blogs');
 
     response.body.forEach((element) => {
       expect(element.id).toBeDefined();
     });
+  });
+
+  test('can be created', async () => {
+    jest.setTimeout(10000);
+
+    const payload = {
+      title: 'Why does my back hurt?',
+      author: 'Mama',
+      url: 'url.com',
+      likes: 4129,
+    };
+
+    await api.post('/api/blogs').send(payload).expect(201);
+
+    const response = await api.get('/api/blogs');
+
+    const blogs = response.body;
+    const lastBlog = blogs[blogs.length - 1];
+    delete lastBlog.id;
+
+    expect(blogs.length).toEqual(helper.initialBlogs.length + 1);
+    expect(payload).toEqual(lastBlog);
   });
 });
 
