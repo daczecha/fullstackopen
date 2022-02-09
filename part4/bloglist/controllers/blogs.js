302f03d11b87c2f85881290f93b870f4e0a185b2
daczecha <daczecha@gmail.com>
2022-02-09 19:32:55 +0400

    bloglist expansions, step2

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -26,6 +26,19 @@ blogsRouter.post('/', async (request, response) => {
   }
 });
 
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = {
+    author: request.body.author,
+    title: request.body.title,
+    url: request.body.url,
+    likes: request.body.likes,
+  };
+  const result = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(result);
+});
+
 blogsRouter.delete('/:id', async (request, response) => {
   const blogs = await Blog.findByIdAndDelete(request.params.id);
   response.status(204).json(blogs);
